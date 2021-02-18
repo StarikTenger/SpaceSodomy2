7dcaf285f9e02ca74edf9c5f1b9e618f7210d017
mea25 <mea25@DESKTOP-QG7R3BI>
2021-02-18 18:20:27 +0300

    Network fix

--- Network/Network/Server_Network.cpp
+++ Network/Network/Server_Network.cpp
@@ -1,71 +1,56 @@
-#include "pch.h"
-#include "Client_Network.h"
+#include <pch.h>
+#include "Server_Network.h"
 
-int Client_Network::getMilliCount() {
-	timeb tb;
-	ftime(&tb);
-	int nCount = tb.millitm + (tb.time & 0xfffff) * 1000;
-	return nCount;
-}
-
-Client_Network::Client_Network() {
+Server_Network::Server_Network() {
+	socket.bind(8001);
 	socket.setBlocking(0);
 }
 
-Client_Network::Client_Network(std::string serverIP_, int port_, int id_, std::string name_) {
-	port = port_;
-	serverIP = serverIP_;
-	id = id_;
-	name = name_;
+Server_Network::Server_Network(int port_) {
+	socket.bind(port_);
 	socket.setBlocking(0);
 }
 
-void Client_Network::change_port(int port_) {
-	port = port_;
+std::deque<std::string> Server_Network::get_messages() {
+	return messages;
 }
 
-void Client_Network::change_server(std::string serverIP_) {
-	serverIP = serverIP_;
+std::string Server_Network::get_last_message() {
+	if (messages.empty())
+		return "";
+	return messages.back();
 }
 
-void Client_Network::change_id(int id_) {
-	id = id_;
+void Server_Network::del_last_message() {
+	if (!messages.empty())
+		messages.pop_back();
 }
 
-void Client_Network::change_name(std::string name_) {
-	name = name_;
+void Server_Network::clear_messages() {
+	messages.clear();
 }
 
-int Client_Network::get_port() {
-	return port;
+std::set<sf::IpAddress> Server_Network::get_addresses() {
+	return addresses;
 }
 
-std::string Client_Network::get_serverIP() {
-	return serverIP;
-}
+void Server_Network::receive() {
+	// Receiving
+	socket.receive(buffer, sizeof(buffer), received, sender, port);
+	if (received)
+	{
+		// Applying sender info
+		addresses.insert(sender);
+		ports[sender] = port;
 
-int Client_Network::get_id() {
-	return id;
-}
-
-std::string Client_Network::get_name() {
-	return name;
-}
-
-void Client_Network::send(std::string data) {
-	data = std::to_string(id) + " " +
-		std::to_string(getMilliCount()) + " " +
-		name + " #" +
-		data;
-	socket.send(data.c_str(), data.size() + 1, serverIP, port);
-}
-
-std::string Client_Network::receive() {
-	for (int i = 0; i < 10; i++) {
-		std::size_t received = 0;
-		sf::IpAddress sender;
-		unsigned short port_ = 0;
-		socket.receive(buffer, sizeof(buffer), received, sender, port_);
+		// Applying received info
+		messages.push_back(std::string(buffer));
+		received = 0;
 	}
-	return std::string(buffer);
+}
+
+void Server_Network::send(std::string message) {
+	for (auto addr : addresses) {
+		socket.send(message.c_str(), message.size() + 1, addr, ports[addr]);
+	}
 }
--- Server/Server/main.cpp
+++ Server/Server/main.cpp
@@ -6,4 +6,10 @@
 int main() {
 	Ship game;
 
+	//Network expample
+	Client_Network client;
+	Server_Network server;
+	client.send("Hello");
+	server.receive();
+	std::cout << server.get_last_message();
 }
