6068350803b0c1ea9aa54b4db666c517a8e9f76c
StarikTenger <StarikTenger@gmail.com>
2021-02-21 16:46:10 +0300

    build issues fixed

--- SpaceSodomy2/Network/Server_Network.h
+++ SpaceSodomy2/Network/Server_Network.h
@@ -1,11 +1,11 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS
 #define _CRT_NONSTDC_NO_DEPRECATE
+#include <SFML/Network.hpp>
 #include <fstream>
 #include <cstdio>
 #include <windows.h>
 #include <vector>
-#include <SFML/Network.hpp>
 #include <iostream>
 #include <cstdlib>
 #include <set>
