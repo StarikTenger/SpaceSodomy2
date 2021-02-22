5a8cc515aff0cd905c7df0d724d674eae8dc9b3a
mea25 <mea25@DESKTOP-QG7R3BI>
2021-02-22 15:52:22 +0300

    Class Player

--- SpaceSodomy2/Game/Ship.h
+++ SpaceSodomy2/Game/Ship.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <box2d/box2d.h>
 #include "Engine.h"
+#include "Player.h"
 
 class Ship {
 private:
-	int id = 0;
-
 	// Entities
+	Player* player;
 	Command_Module* command_module = nullptr;
 	Engine* engine = nullptr;
 	b2Body* body = nullptr;
@@ -16,13 +16,13 @@ public:
 	~Ship();
 
 	// Get methods
-	int get_id();
+	Player* get_player();
 	Command_Module* get_command_module();
 	Engine* get_engine();
 	b2Body* get_body();
 
 	// Set methods
-	void set_id(int val);
+	void set_player(Player*);
 	void set_command_module(Command_Module*);
 	void set_engine(Engine*);
 	void set_body(b2Body*);
