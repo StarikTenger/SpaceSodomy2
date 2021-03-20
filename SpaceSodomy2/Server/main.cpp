e5dcc91fd216363a0af3b1ddf46a4b4db4e14a41
Robby-the-paladin <mea250802@gmail.com>
2021-03-20 13:01:43 +0300

    Background + stamina bar + random color

--- SpaceSodomy2/Server/main.cpp
+++ SpaceSodomy2/Server/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <AuxLib/AuxLib.h>
 #include "Control.h"
 
 int main() {
 	Control control;
-
+	aux::random_seed(aux::get_milli_count());
 	while (1) {  
 		control.step();
 	}
