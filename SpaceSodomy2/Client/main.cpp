eecefc6d2bf7d884078324e872089200532af9c2
StarikTenger <StarikTenger@gmail.com>
2021-04-03 01:14:41 +0300

    Client: random seed

--- SpaceSodomy2/Client/main.cpp
+++ SpaceSodomy2/Client/main.cpp
@@ -1,9 +1,9 @@
 #include "Control.h"
 
 int main() {
+	aux::random_seed(aux::get_milli_count());
 	Control control;
 	control.load_config("client_config.conf");
-	aux::random_seed(aux::get_milli_count());
 	while (control.get_is_running()) {
 		control.step();
 	}
