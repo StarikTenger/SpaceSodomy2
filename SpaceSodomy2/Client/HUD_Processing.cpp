2a12b4a7882e64dd480fe6ac95a32a502022e400
Robby-the-paladin <71353456+Robby-the-paladin@users.noreply.github.com>
2021-06-22 20:03:55 +0300

    replay problem fixed

--- SpaceSodomy2/Client/HUD_Processing.cpp
+++ SpaceSodomy2/Client/HUD_Processing.cpp
@@ -381,46 +381,48 @@ void HUD_Processing::step() {
 			ping_text.step();
 			ping.step();
 		}
-		HP_bar.step();
-		stamina_bar.step();
-		energy_bar.step();
-		interface_image.primitive_step();
-		bonus.primitive_step();
-		bonus_tip.step();
-		auto mtype = game->get_ship(player_network->get_id())->get_left_module()->get_type();
-		auto stam_param = game->get_module_manager()->get_prototype(mtype)->stamina_cost;
-		auto ener_param = game->get_module_manager()->get_prototype(mtype)->energy_cost;
-		if (stam_param >
-			game->get_ship(player_network->get_id())->get_stamina()->get() ||
-			ener_param >
-			game->get_ship(player_network->get_id())->get_energy()->get())
-			left_module.set_color(sf::Color(150, 150, 150, 255));
-		else
-			left_module.set_color(sf::Color::White);
-		left_module.primitive_step();
-		left_module_tip.step();
-		mtype = game->get_ship(player_network->get_id())->get_right_module()->get_type();
-		stam_param = game->get_module_manager()->get_prototype(mtype)->stamina_cost;
-		ener_param = game->get_module_manager()->get_prototype(mtype)->energy_cost;
-		if (stam_param >
-			game->get_ship(player_network->get_id())->get_stamina()->get() ||
-			ener_param >
-			game->get_ship(player_network->get_id())->get_energy()->get())
-			right_module.set_color(sf::Color(150, 150, 150, 255));
-		else
-			right_module.set_color(sf::Color::White);
-		right_module.primitive_step();
-		right_module_tip.step();
-		gun_image.primitive_step();
-		if (game->get_ship(player_network->get_id())->get_left_module()->get_recharge_counter()->get() > b2_epsilon) {
-			draw->fill_polygon(get_vertices(game->get_ship(player_network->get_id())->get_left_module()->get_recharge_counter()->get() /
-				game->get_ship(player_network->get_id())->get_left_module()->get_recharge_counter()->get_max(),
-				left_module.window_cords_pos(), left_module.get_screen_mode() * left_module.get_scale()), sf::Color(0, 0, 0, 150));
-		}
-		if (game->get_ship(player_network->get_id())->get_right_module()->get_recharge_counter()->get() > b2_epsilon) {
-			draw->fill_polygon(get_vertices(game->get_ship(player_network->get_id())->get_right_module()->get_recharge_counter()->get() /
-				game->get_ship(player_network->get_id())->get_right_module()->get_recharge_counter()->get_max(),
-				right_module.window_cords_pos(), right_module.get_screen_mode() * right_module.get_scale()), sf::Color(0, 0, 0, 150));
+		if (game->get_ship(player_network->get_id()) != nullptr) {
+			HP_bar.step();
+			stamina_bar.step();
+			energy_bar.step();
+			interface_image.primitive_step();
+			bonus.primitive_step();
+			bonus_tip.step();
+			auto mtype = game->get_ship(player_network->get_id())->get_left_module()->get_type();
+			auto stam_param = game->get_module_manager()->get_prototype(mtype)->stamina_cost;
+			auto ener_param = game->get_module_manager()->get_prototype(mtype)->energy_cost;
+			if (stam_param >
+				game->get_ship(player_network->get_id())->get_stamina()->get() ||
+				ener_param >
+				game->get_ship(player_network->get_id())->get_energy()->get())
+				left_module.set_color(sf::Color(150, 150, 150, 255));
+			else
+				left_module.set_color(sf::Color::White);
+			left_module.primitive_step();
+			left_module_tip.step();
+			mtype = game->get_ship(player_network->get_id())->get_right_module()->get_type();
+			stam_param = game->get_module_manager()->get_prototype(mtype)->stamina_cost;
+			ener_param = game->get_module_manager()->get_prototype(mtype)->energy_cost;
+			if (stam_param >
+				game->get_ship(player_network->get_id())->get_stamina()->get() ||
+				ener_param >
+				game->get_ship(player_network->get_id())->get_energy()->get())
+				right_module.set_color(sf::Color(150, 150, 150, 255));
+			else
+				right_module.set_color(sf::Color::White);
+			right_module.primitive_step();
+			right_module_tip.step();
+			gun_image.primitive_step();
+			if (game->get_ship(player_network->get_id())->get_left_module()->get_recharge_counter()->get() > b2_epsilon) {
+				draw->fill_polygon(get_vertices(game->get_ship(player_network->get_id())->get_left_module()->get_recharge_counter()->get() /
+					game->get_ship(player_network->get_id())->get_left_module()->get_recharge_counter()->get_max(),
+					left_module.window_cords_pos(), left_module.get_screen_mode() * left_module.get_scale()), sf::Color(0, 0, 0, 150));
+			}
+			if (game->get_ship(player_network->get_id())->get_right_module()->get_recharge_counter()->get() > b2_epsilon) {
+				draw->fill_polygon(get_vertices(game->get_ship(player_network->get_id())->get_right_module()->get_recharge_counter()->get() /
+					game->get_ship(player_network->get_id())->get_right_module()->get_recharge_counter()->get_max(),
+					right_module.window_cords_pos(), right_module.get_screen_mode() * right_module.get_scale()), sf::Color(0, 0, 0, 150));
+			}
 		}
 	}
 	table_step();
