c825e35403be16f9ce8f5ce6d18b604f9225efde
Robby-the-paladin <71353456+Robby-the-paladin@users.noreply.github.com>
2021-12-04 04:31:41 +0300

    TGUI connected

--- SpaceSodomy2/Client/main.cpp
+++ SpaceSodomy2/Client/main.cpp
@@ -1,10 +1,35 @@
-#include "Control.h"
+#include <TGUI/TGUI.hpp>
+#include <TGUI/Backend/SFML-Graphics.hpp>
 
-int main() {
-	aux::random_seed(aux::get_milli_count());
-	Control control;
-	control.load_config("client_config.conf");
-	while (control.get_is_running()) {
-		control.step();
-	}
+int main()
+{
+    sf::RenderWindow window{ {800, 600}, "TGUI example - SFML_GRAPHICS backend" };
+    tgui::Gui gui{ window };
+    tgui::Button::Ptr button = tgui::Button::create();
+    auto x = tgui::BoxLayout::create();
+    auto editBox = tgui::EditBox::create();
+    editBox->setPosition(20, 0);
+    x->setPosition(20, 20);
+    x->add(button);
+    x->add(editBox);
+    gui.add(x);
+    //gui.add(button);
+    //gui.add(editBox, "MyWidgetName");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            gui.handleEvent(event);
+
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+        gui.draw();
+
+        window.display();
+    }
 }
