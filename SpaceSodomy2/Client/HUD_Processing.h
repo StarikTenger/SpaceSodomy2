32499538e69ca0abd5671a1f21ec1f1bf63889db
Robby-the-paladin <71353456+Robby-the-paladin@users.noreply.github.com>
2022-01-27 09:27:46 +0300

    network info + rating table

--- SpaceSodomy2/Client/HUD_Processing.h
+++ SpaceSodomy2/Client/HUD_Processing.h
@@ -3,23 +3,11 @@
 #include<Client/Game_Client.h>
 #include<AuxLib/AuxLib.h>
 #include<Network/Client_Network.h>
-#include <TGUI/TGUI.hpp>
-#include <TGUI/Backend/SFML-Graphics.hpp>
+#include<TGUI/TGUI.hpp>
+#include<TGUI/Backend/SFML-Graphics.hpp>
 
 class HUD_Processing {
 private:
-	Bar HP_bar, stamina_bar, energy_bar;
-	Constant_Text time_to_respawn, press_r_to_respawn,
-		ping, ping_text, fps, fps_text;
-	Menu_Object bonus, left_module, right_module, gun_image;
-	Text_Field bonus_tip, left_module_tip, right_module_tip;
-
-	int table_use_windows_cords;
-	b2Vec2 table_pos;
-	float table_character_size;
-	float table_name_indent;
-	b2Vec2 table_indent;
-
 	Game_Client* game;
 	Client_Network* player_network;
 
@@ -30,16 +18,8 @@ private:
 	std::string bonus_button_name, left_module_button_name, right_module_button_name, respawn_button_name;
 
 	void get_buttons_names(tgui::Gui &gui, std::string path);
-	void apply_bar(Bar* bar, std::stringstream* config);
-	void table_step();
+	void table_step(tgui::Gui &gui, float scale);
 
-	int HP_bar_max_val;
-	int stamina_bar_max_val;
-	float HP_bar_val;
-	float stamina_bar_val;
-	int energy_bar_max_val;
-	float energy_bar_val;
-	float text_mod = 1;
 	std::queue<int>* frame_marks;
 
 	std::vector<b2Vec2> get_vertices(float cur_pos, b2Vec2 center, b2Vec2 scale);
