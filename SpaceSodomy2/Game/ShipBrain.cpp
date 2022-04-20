f03e0acc3702b4aa7c6721f5343834496c7cbaa4
Alex THe Polygonal <alexthepolygonal@gmail.com>
2022-04-20 20:01:58 +0300

    clean

--- SpaceSodomy2/Game/ShipBrain.cpp
+++
@@ -1,6 +0,0 @@
-#include "pch.h"
-#include "ShipBrain.h"
-
-
-ShipBrain::ShipBrain(CommandModule& a, const GameReadable& b, Ship* ptr) : iBrain(a, b), ship(ptr) {}
-void ShipBrain::set_ship(Ship* _) { ship = _; }
