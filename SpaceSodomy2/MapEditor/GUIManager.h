a8b7a2291c54bd38c1edca04bd2ed06ab221aa7a
Robby-the-paladin <71353456+Robby-the-paladin@users.noreply.github.com>
2022-05-11 22:55:17 +0300

    GUIManager init

---
+++ SpaceSodomy2/MapEditor/GUIManager.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <TGUI/TGUI.hpp>
+#include <TGUI/Backend/SFML-Graphics.hpp>
+class GUIManager
+{
+private:
+
+public:
+	void init(tgui::Gui& gui);
+};
+
