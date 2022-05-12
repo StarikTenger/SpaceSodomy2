62806c34d52b6d70f202a2d46308730d3b2155f9
StarikTenger <StarikTenger@gmail.com>
2022-05-12 11:54:28 +0300

    Game.h cleanup

--- SpaceSodomy2/Game/Game.h
+++ SpaceSodomy2/Game/Game.h
@@ -26,37 +26,18 @@
 
 class Game : public GameReadable {
 protected:
-	bool auto_damage = 0;
-	b2Vec2 get_rand_respawn_pos();
-
-
-	// Time step
-	//float dt = 0.02;
-	//float time = 0;
+	// Returns random position suitable for placing a new ship
+	b2Vec2 get_rand_respawn_pos(); // TODO: respawn manager
 
 	// Objects' systems
 	std::map<int, Player*> players;
 	std::map<int, int>* connection_time;
 	std::set<int> id_list;
-	// std::set<Ship*> ships;
-	//std::set<Engine*> engines;
-	//std::set<CommandModule*> command_modules;
-	
-	// std::set<ActiveModule*> active_modules;
-	// std::set<Projectile*> projectiles;
-	//std::set<Counter*> counters;
-	//std::set<DamageReceiver*> damage_receivers;
-	//std::set<Event*> events;
-	//std::set<Effects*> effects;
-	//std::set<Bonus*> bonuses;
-	//std::set<Rocket*> rockets;
 	std::set<RocketBrain*> rocket_brains;
-	//std::set<Forcefield*> forcefields;
 	
 	// Walls
-	// std::set<Wall*> walls;
 	b2Vec2 lower_left_corner, upper_right_corner;
-	Player* wall_player = nullptr; // id = -1;
+	Player* wall_player = nullptr; // id = -1; TODO: or not todo
 
 	// Managers
 	ProjectileManager projectile_manager;
@@ -65,12 +46,10 @@ protected:
 	BonusManager bonus_manager;
 	ModuleManager module_manager;
 	RocketManager rocket_manager;
-	
-	// b2World physics = b2World(b2Vec2_zero);
 
-	// Collision control
+	// Determines which collisions are allowed and which are ignored
 	CollisionFilter collision_filter;
-	// Contact table (stores pairs which are in contact)
+	// Contact table (stores body pairs which are in contact)
 	ContactTable contact_table;
 
 	// Ship components
@@ -91,17 +70,17 @@ protected:
 	Player*          create_player(int id, sf::Color color = {}, std::string name = "_");
 	b2Body*          create_round_body(b2Vec2 pos, float angle, float radius, float mass);
 	Gun*             create_gun(Gun_Prototype);
-	CommandModule*  create_command_module();
+	CommandModule*   create_command_module();
 	Engine*          create_engine(b2Body* = nullptr, CommandModule* = nullptr, Counter* = nullptr, Effects* = nullptr, float stamina_delay_ = 0.7);
 	Counter*         create_counter(float val = 0, float change_vel = 0);
-	DamageReceiver* create_damage_receiver(b2Body* = nullptr, Counter* = nullptr, Player* = nullptr, Effects* = nullptr);
+	DamageReceiver*  create_damage_receiver(b2Body* = nullptr, Counter* = nullptr, Player* = nullptr, Effects* = nullptr);
 	Ship*            create_ship(Player* player, b2Vec2 pos, float angle);
 	Wall*            create_wall(std::vector<b2Vec2> vertices, int orientation = Wall::OUTER, float restitution = 0.5, int type = Wall::STANDART);
 	Projectile*      create_projectile(ProjectileDef);
 	Event*           create_event(EventDef, float playing_offset = 0);
 	Effects*         create_effects(Effects_Prototype*);
 	Bonus*           create_bonus(Bonus_Def);
-	BonusSlot*      create_bonus_slot();
+	BonusSlot*       create_bonus_slot();
 	Module*          create_module(Module_Prototype*);
 	Rocket*          create_rocket(Rocket_Def);
 	Forcefield*      create_forcefield(std::vector<b2Vec2> vertices, b2Vec2 force);
@@ -146,6 +125,7 @@ protected:
 	b2Vec2 get_beam_intersection(b2Vec2 start, float angle);
 
 public:
+	// Returns readble interface
 	GameReadable& get_readable();
 
 	Game();
@@ -171,6 +151,7 @@ protected:
 	void create_new_player(int id, sf::Color color, std::string name, std::string gun_name, std::string hull_name,
 		std::string left_module, std::string right_module);
 public:
+	// Creates new player, returns true if success
 	bool new_player(PlayerDef);
 	// Gets player by id
 	Player* player_by_id(int id);
@@ -178,4 +159,3 @@ public:
 	void delete_player(int id);
 	~Game();
 };
-
