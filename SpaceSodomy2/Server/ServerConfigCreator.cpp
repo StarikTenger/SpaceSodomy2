9b7c65513bc6e73951eaf0aead6cd202fba2299e
StarikTenger <StarikTenger@gmail.com>
2022-06-21 21:37:54 +0300

    ServerConfigCreator

--- SpaceSodomy2/Server/ServerConfigCreator.cpp
+++ SpaceSodomy2/Server/ServerConfigCreator.cpp
@@ -1,14 +1,11 @@
+#include "ServerConfigCreator.h"
+
+void ServerConfigCreator::run() {
+	{write_config("bot_list.conf", R"(
 BOT
     BOT_TYPE EDGAR_BOT
     GUN_NAME heavy
     HULL_NAME heavy
-    LEFT_MODULE_NAME NONE
-    RIGHT_MODULE_NAME NONE
-END
-BOT
-    BOT_TYPE EDGAR_BOT
-    GUN_NAME heavy
-    HULL_NAME light
     LEFT_MODULE_NAME NONE
     RIGHT_MODULE_NAME NONE
 END
@@ -20,3 +17,6 @@ BOT
     RIGHT_MODULE_NAME NONE
 END
 END
+
+		)"); }
+}
--- SpaceSodomy2/Server/main.cpp
+++ SpaceSodomy2/Server/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <AuxLib/AuxLib.h>
 #include "Control.h"
+#include "ServerConfigCreator.h"
 
 int main() {
+	// Create configs
+	ServerConfigCreator config_creator;
+	config_creator.run();
+
 	aux::random_seed(aux::get_milli_count());
 	Control control;
 	while (1) {  
